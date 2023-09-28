de0745faeb075fbba7f05eaef1df3b56634d0ea3
Theo Rodriguez <rodriguez.theo026@gmail.com>
2023-09-28 23:14:14 +0200

    Fix prisma call

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-    return new PrismaClient()
-}
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: `${process.env.DATABASE_URL}?connection_limit=20`
+    }
+  }
+});
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClientSingleton | undefined
-}
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export default prisma;
